e66d80a72ac03294fca158d0b00906c4db9b8cb4
aoustimov <aoustimov@gmail.com>
2021-05-07 14:07:37 +0000

    adding code

--- rbm_run.py
+++ rbm_run.py
@@ -17,5 +17,17 @@ parser.add_argument('--save', type=bool, default=True, help='saves model and che
 parser.add_argument('--load', type=bool, default=False, help='loads model and checkpoints')
 parser.add argument('--dist_type_vis', type=str, default='bernoulli', help='visible distribution type.')
 parser.add_argument('--dist_type_hid', type=str, default='bernoulli', help='hidden distribution type.')
+parser.add_argument('--cd_k', type=int, default=3, help='number of cd interations')
+parser.add argument('--epoch',type=int, default=1, help='nuber of training loops')
+parser.add argument('--v_marg_steps', type=int, default=250, help='number of sampling iterations')
+parser.add argument('--lr', type=float, default=1e-4, help='learning rate')
+parser.add argument('--use_tuner', type=bool, default=False, help='use keras hyperparameter_tuner')
+parser.add argument('--train_using_tuner', type=bool, default=False, help='use resuls from keras tuner to train new model')
+parser.add argument('--optimizer', type=str, default="SGD", help='choose to use Adam of SGD')
+
+parser.add argument('--n_bernoulli', type=int, dfault=100, help='number of bernoulli visible units')
+parser.add argument('--n_cat_sm', type=int, dfault=4, help='number of softmax categories in each unit')
+parser.add argument('--n_softmax', type=int, dfault=100, help='number of softmax visible units')
+parser.add argument('--n_hidden', type=int, dfault=10, help='number of bernoulli hidden units')
 
 
