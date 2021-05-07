e2b74c1ef49915c8d967b161868f775e885c4497
aoustimov <aoustimov@gmail.com>
2021-05-07 14:32:21 +0000

    adding code

--- rbm_run.py
+++ rbm_run.py
@@ -15,19 +15,68 @@ parser.add_argument('--checkpoint_path', type=str,
 default="log directory/check_{epoch}/cp-{epoch:02d}.ckpt", help="path to save model")
 parser.add_argument('--save', type=bool, default=True, help='saves model and checkpoints')
 parser.add_argument('--load', type=bool, default=False, help='loads model and checkpoints')
-parser.add argument('--dist_type_vis', type=str, default='bernoulli', help='visible distribution type.')
+parser.add_argument('--dist_type_vis', type=str, default='bernoulli', help='visible distribution type.')
 parser.add_argument('--dist_type_hid', type=str, default='bernoulli', help='hidden distribution type.')
 parser.add_argument('--cd_k', type=int, default=3, help='number of cd interations')
-parser.add argument('--epoch',type=int, default=1, help='nuber of training loops')
-parser.add argument('--v_marg_steps', type=int, default=250, help='number of sampling iterations')
-parser.add argument('--lr', type=float, default=1e-4, help='learning rate')
-parser.add argument('--use_tuner', type=bool, default=False, help='use keras hyperparameter_tuner')
-parser.add argument('--train_using_tuner', type=bool, default=False, help='use resuls from keras tuner to train new model')
-parser.add argument('--optimizer', type=str, default="SGD", help='choose to use Adam of SGD')
+parser.add_argument('--epoch',type=int, default=1, help='nuber of training loops')
+parser.add_argument('--v_marg_steps', type=int, default=250, help='number of sampling iterations')
+parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+parser.add_argument('--use_tuner', type=bool, default=False, help='use keras hyperparameter_tuner')
+parser.add_argument('--train_using_tuner', type=bool, default=False, help='use resuls from keras tuner to train new model')
+parser.add_argument('--optimizer', type=str, default="SGD", help='choose to use Adam of SGD')
 
-parser.add argument('--n_bernoulli', type=int, dfault=100, help='number of bernoulli visible units')
-parser.add argument('--n_cat_sm', type=int, dfault=4, help='number of softmax categories in each unit')
-parser.add argument('--n_softmax', type=int, dfault=100, help='number of softmax visible units')
-parser.add argument('--n_hidden', type=int, dfault=10, help='number of bernoulli hidden units')
+parser.add_argument('--n_bernoulli', type=int, dfault=100, help='number of bernoulli visible units')
+parser.add_argument('--n_cat_sm', type=int, dfault=4, help='number of softmax categories in each unit')
+parser.add_argument('--n_softmax', type=int, dfault=100, help='number of softmax visible units')
+parser.add_argument('--n_hidden', type=int, dfault=10, help='number of bernoulli hidden units')
+parser.add_argument('--proba_activation_toggle', type=str, default="not_activation",
+                    help='changing bias to stochastic activation values')
+parser.add_argument('--l2_param', type=float, default=0.0001, help='l2 param')
+parser.add_argument('--lr_red_factor', type=float, default=0.5, help='lr_red_factor')
+parser.add_argument('--lr_b', type=float, default=0.0015, help='lr for bernoulli weights')
+parser.add_argument('--lr_s', type=float, default=0.0015, help='lr for somftmax wieghts')
+parser.add_argument('--lr_b_bias', type=float, default=0.0015, help='lr for bernoulli bais')
+parser.add_argument('--lr_s_bias', type=float, default=0.0015, help='lr for softmax bias')
+parser.add_argument('--lr_h_bias', type=float, default=0.0015, help='lr for the hidden bias')
+parser.add_argument('--loss_type', type=str, default='fe_reconstruction_cost', help='loss type')
+
+args = parser.parse_args()
+
+## building model for tuner
+def build_model(hp):
+    # number of hidden units
+    hp_n_hid = hp.Int('n_hidden', min_value=1000, max_value=1010, step=10)
+    args.n_hidden = hp_n_hid
+    # learning rates
+    hp_lr = hp.Choice('learning_rate', values=[1e-2,1e-3, 1e-4])
+    args.lr = hp_lr
+    hp_l2_param = hp.Choice('l2_param', values=[1e-2,1e-3, 1e-4])
+    args.l2_param = hp_l2_param
+    hp_lr_red_factor = hp.Choice('lr_red_factor', values=[0.5, 0.25])
+    args.lr_red_factor  = hp_lr_red_factor
+    hp_lr_b = hp.Choice('lr_b', values=[1e-2,1e-3, 1e-4])
+    args.lr_b = hp_lr_b
+    hp_lr_s = hp.Choice('lr_s', values=[1e-2,1e-3, 1e-4])
+    args.lr_s = hp_lr_s
+    hp_lr_b_bias = hp.Choice('lr_b_bias', values=[1e-2,1e-3, 1e-4])
+    args.lr_b_bias = hp_lr_b_bias
+    hp_lr_s_bias = hp.Choice('lr_s_bias', values=[1e-2,1e-3, 1e-4])
+    args.lr_s_bias = hp_lr_s_bias
+    hp_lr_h_bias = hp.Choice('lr_h_bias', values=[1e-2,1e-3, 1e-4])
+    args.lr_h_bias = hp_lr_h_bias
+
+    rbm(RBM(args))
+    rbm.compile(optimizer-tf.keras.optimizers.SGD(learning_rate=1e-3))
+    return rbm
+
+##
+Class MyTuner(kt.Tuner):
+    def run_trial(self, trial, train_ds):
+        hp = trial.hyperparameters
+        train_ds = train_ds.batch(10, drop_remainder=True)
+    
+
+
+
 
 
